940514b99ad3f4db14be26023c8df15adc0f1f3c
Yura <ogogan45@gmail.com>
2023-09-26 13:23:09 +0300

    fix

--- src/shared/ui/Notification/Notification.tsx
+++ src/shared/ui/Notification/Notification.tsx
@@ -1,5 +1,6 @@
 import {type FC, useEffect} from "react";
-import {CSSTransition} from "react-transition-group";
+import {CSSTransition} from 'react-transition-group';
+
 
 type Props = {
     children: React.ReactNode;
