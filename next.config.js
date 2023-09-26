55a7d0dc0485050843a3a4b3f599f58464d66326
Yura <ogogan45@gmail.com>
2023-09-26 13:51:42 +0300

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-/** @type {import("next").NextConfig} */
-module.exports = {
-	reactStrictMode: false,
-	images: {
-		domains: ["images.unsplash.com", "s3.ap-southeast-1.amazonaws.com", "miro.medium.com", "uploadthing.com", "utfs.io"],
-	},
-	experimental: {
-		esmExternals: false, // THIS IS THE FLAG THAT MATTERS
-	},
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['images.unsplash.com', 's3.ap-southeast-1.amazonaws.com', 'miro.medium.com', 'uploadthing.com', 'utfs.io']
+    },
+    experimental: {
+        esmExternals: false, // THIS IS THE FLAG THAT MATTERS
+    },
+}
+
+module.exports = nextConfig
